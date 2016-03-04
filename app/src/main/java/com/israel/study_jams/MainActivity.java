d07029e32a98014b9896d729663fc74a48bf1b93
yzraeu <yzraeu@gmail.com>
2016-03-04 14:10:28 -0300

    First 2 lessons

---
+++ app/src/main/java/com/israel/study_jams/MainActivity.java
@@ -0,0 +1,13 @@
+package com.israel.study_jams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
